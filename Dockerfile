46858aab0c0685b4c62c4354df8829ed428a0cf0
Nasser Dmeiry <nasser.dmeiry@gmail.com>
2024-09-17 08:11:24 +0300

    add the code -all and cloudbuild yaml file and fix and add .env file

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM node:22
 WORKDIR /skitick
 COPY . .
 RUN npm install
-ENV MONGODB_URI="mongodb+srv://anas:zhphfYZsFG04ccgJ@cluster0.pqnw4.mongodb.net/skitick?retryWrites=true&w=majority&appName=Cluster0"
 EXPOSE 3000
 CMD ["npm", "start", "dev"]
--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect("mongodb+srv://anas:zhphfYZsFG04ccgJ@cluster0.pqnw4.mongodb.net/skitick?retryWrites=true&w=majority&appName=Cluster0");
-       // const db = await mongoose.connect(process.env.MONGO_URI);
+        const db = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected : ${db.connection.host}`);
     } catch (error) {
         console.error(error);
