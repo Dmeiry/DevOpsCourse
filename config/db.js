0715a335a1b6dc488de5ebc42cf14357cb65f55b
Nasser Dmeiry <nasser.dmeiry@gmail.com>
2024-09-18 10:49:34 +0300

    checking if env is here and fixing db connection by adding secrets

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
+        const db = await mongoose.connect(MONGO_URI);
         console.log(`MongoDB Connected : ${db.connection.host}`);
     } catch (error) {
         console.error(error);
